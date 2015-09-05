d94d7644327a2213c502e7983ba0a1d58e2fd5d0
esmyser <esmyser@gmail.com>
2015-09-05 14:26:05 -0400

    opens twitter connection

---
+++ app/models/twitter_wrapper.rb
@@ -0,0 +1,17 @@
+class TwitterWrapper
+
+  def initialize(user)
+     @user = user
+     @client = client
+  end
+
+  def client
+    Twitter::REST::Client.new do |config|
+      config.consumer_key        = ENV["twitter_key"]
+      config.consumer_secret     = ENV["twitter_secret"]
+      config.access_token        = ENV["twitter_access_token"]
+      config.access_token_secret = ENV["twitter_access_token_secret"]
+    end
+  end
+
+end
