e0cc881e8c560d2bf6fa05b91da089b3ca886df3
esmyser <esmyser@gmail.com>
2015-09-07 17:54:25 -0400

    refactors NycSentiment

--- app/models/nyc_sentiment.rb
+++ app/models/nyc_sentiment.rb
@@ -1,12 +1,9 @@
 class NycSentiment
 
-  attr_accessor :hour_tweets, :sentiment
+  attr_accessor :sentiment
 
   def initialize
-    nyc = {result_type: "recent", geocode: "40.7731295,-73.957734,5mi", locale: "en", count: 100, include_rts: false}
-    @hour_tweets = TwitterWrapper.new('esmyser').last_hour_tweets(nyc)
     @sentiment = sentiment
-    save_hour_tweets
   end
 
   def sentiment
@@ -14,10 +11,19 @@ class NycSentiment
     Sentimental.new(0.1)
   end
 
-  def save_hour_tweets
-    @hour_tweets.each do |tweet|
+  def nyc
+    {result_type: "recent", geocode: "40.7731295,-73.957734,5mi", locale: "en", count: 100, include_rts: false}
+  end
+
+  def tweets
+    TwitterWrapper.new('esmyser').last_hour_tweets(nyc)
+  end
+
+  def save_tweets
+    tweets.each do |tweet|
       t = Tweet.new
       hydrate_tweet(t, tweet) if useable_tweet(t, tweet)
+      t.save
     end
   end
 
@@ -34,7 +40,6 @@ class NycSentiment
     t.tweet_id = tweet[:id]
     t.lat = tweet[:coordinates][:coordinates].first
     t.lng = tweet[:coordinates][:coordinates].last
-    t.save
   end  
 
 end
